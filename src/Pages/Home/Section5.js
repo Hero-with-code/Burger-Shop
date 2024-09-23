9c86fc3169ba17c67947f3a199b593cdd03bf6fe
Reyaz-Ansari <mdreyaz7079@gmail.com>
2024-09-23 12:42:14 +0530

    update

--- src/Pages/Home/Section5.js
+++ src/Pages/Home/Section5.js
@@ -17,59 +17,58 @@ function Section5() {
             <Row>
             <Col>
                 <div className='abc1 abc pt-1'>
-                <div className='d-flex justify-content-center align-items-center'>
-                            <div>
-                                <i class="bi bi-code-slash"></i>
-                            </div>                           
-                        </div>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <div>
+                            <i class="bi bi-code-slash"></i>
+                                </div>                           
+                            </div>
                         <h3>Modern Design</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
-                </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
+            </div>    
             </Col>
             <Col>
                 <div className='abc2 abc pt-1'>
-                <div className='d-flex justify-content-center align-items-center'>
-                            <div>
-                                <i class="bi bi-code-slash"></i>
-                            </div>                           
-                        </div>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <div>
+                             <i class="bi bi-code-slash"></i>
+                                </div>                           
+                            </div>
                         <h3>Fast Loading</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
                 </div>
             </Col>
             <Col>
                 <div className='abc3 abc pt-1'>
-                <div className='d-flex justify-content-center align-items-center'>
-                            <div>
-                                <i class="bi bi-code-slash"></i>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <div>
+                            <i class="bi bi-code-slash"></i>
                             </div>                           
                         </div>
-                        <h3>Clean Code</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
-                </div>
+                    <h3>Clean Code</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
+            </div>
             </Col>
             </Row>
         </section>
 
         <div className='sect-3'></div>
-
-        <Row className='text-center'>
-            <Col>
-                <h2>1078</h2>
-                <p>Completed Projects</p>
-            </Col>
-            <Col>
-                <h2>250048</h2>
-                <p>Lines Code</p>
-            </Col>
-            <Col>
-                <h2>250</h2>
-                <p>Team Members</p>
-            </Col>
-            <Col>
-                <h2>2045</h2>
-                <p>Happy Clients</p>
-            </Col>
+            <Row className='text-center'>
+                <Col>
+                    <h2>1078</h2>
+                    <p>Completed Projects</p>
+                </Col>
+                <Col>
+                    <h2>250048</h2>
+                    <p>Lines Code</p>
+                </Col>
+                <Col>
+                    <h2>250</h2>
+                    <p>Team Members</p>
+                </Col>
+                <Col>
+                    <h2>2045</h2>
+                    <p>Happy Clients</p>
+                </Col>
             </Row>
 
     </section>
